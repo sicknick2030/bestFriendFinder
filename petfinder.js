c3a53ba4b6f6f083209ff029bb5aa24c455d62b8
Sasha <sashikers@gmail.com>
2018-01-06 08:03:26 -0800

    favorites stored in firebase

--- petfinder.js
+++ petfinder.js
@@ -1,22 +1,23 @@
-// var config = {
-//     apiKey: "AIzaSyBj0VNeS_U8PR-NDxZNYUnfb67Ca76FEzw",
-//     authDomain: "bestfriendfinder-26231.firebaseapp.com",
-//     databaseURL: "https://bestfriendfinder-26231.firebaseio.com",
-//     projectId: "bestfriendfinder-26231",
-//     storageBucket: "bestfriendfinder-26231.appspot.com",
-//     messagingSenderId: "739384275677"
-//   };
+var config = {
+    apiKey: "AIzaSyBj0VNeS_U8PR-NDxZNYUnfb67Ca76FEzw",
+    authDomain: "bestfriendfinder-26231.firebaseapp.com",
+    databaseURL: "https://bestfriendfinder-26231.firebaseio.com",
+    projectId: "bestfriendfinder-26231",
+    storageBucket: "bestfriendfinder-26231.appspot.com",
+    messagingSenderId: "739384275677"
+  };
 
-// firebase.initializeApp(config);
-// var db = firebase.database().ref();
+firebase.initializeApp(config);
+var db = firebase.database().ref();
+var dbFav = firebase.database().ref("/favorites");
 
 
-console.log("hello");
+console.log("wow");
 var PetFinderAPIKey = "156a1b8fa2233c99240e24d804ef4754";
 
 function getPets(event) {
 	event.preventDefault();
-	console.log("getPets");
+	// console.log("getPets");
 
 	var petType = $("#petType").val().trim();
 	var zipCode = $("#zipCode").val().trim();
@@ -47,7 +48,7 @@ function getPets(event) {
 			count: 5,
 		},
 		success: function(petApiData) {
-			console.log(petApiData);
+			// console.log(petApiData);
 			var pets = petApiData.petfinder.pets.pet;
 			console.log("pets", pets);
 
@@ -66,6 +67,10 @@ function getPets(event) {
 				// var animalDescriptionDiv = $("<p>").text(animalDescription);
 				// animalDiv.append(animalDescriptionDiv);
 
+				var animalID = pets[i].id.$t;
+				var favoriteButton = $("<button class='addFavorite' value='" + animalID + "'>").text("Favorite");
+				animalDiv.append(favoriteButton);
+
 				$("#results").append(animalDiv);
 
 				// NEED TO GENERATE A LIST OF SHELTERS
@@ -77,6 +82,101 @@ function getPets(event) {
 
 // $(document).on("click","#petFinderSubmit",getPets);
 $(document).on("change",".target",getPets);
+
+function addFavorite() {
+	var animalID = this.value;
+
+	var queryURL = "https://api.petfinder.com/pet.get";
+	
+	$.ajax({
+		url: queryURL,
+		jsonp: "callback",
+		dataType: "jsonp",
+		data: {
+			key: PetFinderAPIKey,
+			id: animalID,
+			output: "basic",
+			format: "json",
+		},
+		success: function(petResponse) {
+			console.log(petResponse);
+			var pet = petResponse.petfinder.pet;
+			var favoriteName = pet.name.$t;
+			var favoriteShelterID = pet.shelterId.$t;
+			var favoritePhoto = pet.media.photos.photo[2];
+			dbFav.push({
+				favoriteID: animalID,
+				favoriteName: favoriteName,
+				favoriteShelterID: favoriteShelterID,
+				favoritePhoto: favoritePhoto,
+			});
+		}
+	})
+}
+
+$(document).on("click",".addFavorite",addFavorite);
+
+dbFav.on("child_added", function(snapshot) {
+	console.log("child_added", snapshot.val());
+
+	var favID = snapshot.val().favoriteID;
+
+	// var queryURL = "https://api.petfinder.com/pet.get";
+	
+	// $.ajax({
+	// 	url: queryURL,
+	// 	jsonp: "callback",
+	// 	dataType: "jsonp",
+	// 	data: {
+	// 		key: PetFinderAPIKey,
+	// 		id: animalID,
+	// 		output: "basic",
+	// 		format: "json",
+	// 	},
+	// 	success: function(petResponse) {
+	// 		console.log(petResponse);
+	// 		var pets = petApiData.petfinder.pet;
+	// 	}
+	// })
+
+	// var favDiv = $("<div class ='favDiv'>");
+
+	// var animalName = pets[i].name.$t;
+	// var animalNameDiv = $("<h2>").text(animalName);
+	// favDiv.append(animalNameDiv);
+
+	// var animalImageURL = pets[i].media.photos.photo[2].$t;
+	// var animalImage = $("<img class='animal'>").attr("src", animalImageURL);
+	// favDiv.append(animalImage);
+
+	// var animalID = pets[i].id.$t;
+	// var favoriteButton = $("<button class='addFavorite' value='" + animalID + "'>").text("Favorite");
+	// favDiv.append(favoriteButton);
+
+	// $("#favorites").append(favDiv);
+
+})
+
+// 	var queryURL = "https://api.petfinder.com/pet.get";
+	
+// 	$.ajax({
+// 		url: queryURL,
+// 		jsonp: "callback",
+// 		dataType: "jsonp",
+// 		data: {
+// 			key: PetFinderAPIKey,
+// 			id: animalID,
+// 			output: "basic",
+// 			format: "json",
+// 		},
+// 		success: function(petResponse) {
+// 			console.log(petResponse);
+// 			var pets = petApiData.petfinder.pet;
+// 		}
+// 	})
+
+
+
 
 
 // section below is just experimental, but i think we can use it to generate the secondary dropdown (animal > breed)
@@ -103,13 +203,9 @@ function getBreeds() {
 	})
 }
 
-// $(document).on("click","#getBreedList", getBreeds);
-// $(document).on("change",".target", getBreeds);
-
-// $(".target").change(getBreeds);
+$(document).on("click","#getBreedList", getBreeds);
 
 function showShelter()  {
-	// console.log("showShelter");
 	var queryURL = "https://api.petfinder.com/shelter.get";
 	
 	$.ajax({
